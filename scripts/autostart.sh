36a12a2a811976c9129a74563fba12d625fdad7d
foongkev <foongkev@gmail.com>
2018-04-26 00:19:59 +0800

    Organised Xresources better

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,7 +4,7 @@ xset fp+ ~/dotfiles/fonts/tamzen-font-Tamzen-1.11.4/bdf
 xset fp+ ~/dotfiles/fonts/neep
 xset fp+ ~/.local/share/fonts/terminus/
 xset fp rehash
-xrdb -load dotfiles/xresources/Xresources
+xrdb -load ~/dotfiles/xresources/Xresources
 hsetroot -solid '#FFFFFF'
 sxhkd &
 xrandr --output VGA-1 --mode 2560x1440
