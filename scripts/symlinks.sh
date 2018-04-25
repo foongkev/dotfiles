3a6f89d0a0e9cd574719d01c788cbb0637d22c62
foongkev <foongkev@gmail.com>
2018-04-25 22:49:11 +0800

    My first dotfile update

---
+++ scripts/symlinks.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+sudo ln -s ~/dotfiles/zsh/.zprofile ~/
+sudo ln -s ~/dotfiles/zsh/.zshrc ~/
+sudo ln -s ~/dotfiles/sxhkd/ ~/.config
+sudo ln -s ~/dotfiles/polybar/ ~/.config
+sudo ln -s ~/dotfiles/bspwm/ ~/.config
+sudo ln -s ~/dotfiles/compton/compton.conf ~/.config
+sudo ln -s ~/dotfiles/vimrc/.vimrc ~/
