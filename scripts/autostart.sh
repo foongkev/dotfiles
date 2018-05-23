f88bdb9efd49e147d34bf770b1ecbccfb1c14285
foongkev <foongkev@gmail.com>
2018-05-23 19:55:36 +0800

    set DPMS and trackpad settings

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,14 +2,18 @@
 
 # Uncomment this if need to enable tapping on trackpad (if not using a vm)
 # Also type 'xinput list-props "SynPS/2 Synaptics TouchPad" | grep "Synaptics Scrolling Distance"' to get the scrolling distance
-# xinput set-prop "Synaptics TM3072-003" "Synaptics Tap Action" 0 0 0 0 1 3 2
-# xinput set-prop "Synaptics TM3072-003" "Synaptics Scrolling Distance" -114 -114
-# xinput set-prop "Synaptics TM3072-003" "Synaptics Two-Finger Scrolling" 1 1
+xinput set-prop "Synaptics TM3072-003" "Synaptics Tap Action" 0 0 0 0 1 3 2
+xinput set-prop "Synaptics TM3072-003" "Synaptics Scrolling Distance" -114 -114
+xinput set-prop "Synaptics TM3072-003" "Synaptics Two-Finger Scrolling" 1 1
 
 # Load fonts into font database and refresh database
 xset fp+ ~/dotfiles/fonts/tamzen-font-Tamzen-1.11.4/bdf
 xset fp+ ~/dotfiles/fonts/siji
 xset fp rehash
+
+# Display Power Management Signalling settings
+#    standby suspend off
+xset 3600 4800 6000
 
 # Load Xresources
 xrdb -load ~/dotfiles/xresources/Xresources
