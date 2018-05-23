28f3b0fc99cbb450760db446114e5036a59907c1
foongkev <foongkev@gmail.com>
2018-05-24 00:45:07 +0800

    Updated x86 buttons for volume and brightness:wq

---
+++ scripts/symlink_list.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Create symbolic links for below configuration files
+
+#ln -sfv ~/dotfiles/Xresources/Xresources ~/.Xresources
+ln -sfv ~/dotfiles/zsh/zprofile ~/.zprofile
+ln -sfv ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sfv ~/dotfiles/vimrc/vimrc ~/.vimrc
+#ln -sfv ~/dotfiles/polybar/config /home/foongkev/.config/polybar/config
+#ln -sfv ~/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
